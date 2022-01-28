6771b121bc3f7246dcf77ced90bd6da2d4a16971
Gustavo Omonte Acevedo <gustavo.omonte@fundacion-jala.org>
2022-01-28 17:23:22 -0400

    especify node and npm

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
     '/*': '/$1',
-}))
+}));
 server.use(router);
 server.listen(PORT, () => {
     console.log('Server is running');
